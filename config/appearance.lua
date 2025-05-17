b99b8158628e41f190ce660ca54f55ceec8f270c
haikali3 <manfdvcL9@gmail.com>
2025-05-17 11:40:41 +0800

    refactor(config/appearance): remove unused vim cursor styles

--- config/appearance.lua
+++ config/appearance.lua
@@ -18,16 +18,6 @@ return {
    default_cursor_style = 'BlinkingBlock',
    cursor_blink_rate = 650,
 
-   -- vim cursor
-   cursor_styles = {
-      vim_insert = {
-         cursor_style = 'BlinkingBlock',
-      },
-      vim_normal = {
-         cursor_style = 'Block',
-      },
-   },
-
    -- color scheme
    colors = colors,
 
