691fb3619a1087fff98d39bea7019e532b3ef35d
haikali3 <manfdvcl9@gmail.com>
2025-10-24 07:27:52 +0800

    fix: update tab navigation key bindings to use numeric keys

--- config/bindings.lua
+++ config/bindings.lua
@@ -64,10 +64,10 @@ local keys = {
    { key = 'w',          mods = mod.SUPER_REV, action = act.CloseCurrentTab({ confirm = false }) },
 
    -- tabs: navigation
-   { key = '[',          mods = mod.SUPER,     action = act.ActivateTabRelative(-1) },
-   { key = ']',          mods = mod.SUPER,     action = act.ActivateTabRelative(1) },
-   { key = '[',          mods = mod.SUPER_REV, action = act.MoveTabRelative(-1) },
-   { key = ']',          mods = mod.SUPER_REV, action = act.MoveTabRelative(1) },
+   { key = '1',          mods = mod.SUPER,     action = act.ActivateTabRelative(-1) },
+   { key = '2',          mods = mod.SUPER,     action = act.ActivateTabRelative(1) },
+   { key = '1',          mods = mod.SUPER_REV, action = act.MoveTabRelative(-1) },
+   { key = '2',          mods = mod.SUPER_REV, action = act.MoveTabRelative(1) },
 
    -- tab: title
    { key = '0',          mods = mod.SUPER,     action = act.EmitEvent('tabs.manual-update-tab-title') },
