f73ce5afaa20adc01fe0aadb99f345f56dc4ffca
haikali3 <manfdvcl9@gmail.com>
2025-11-13 14:54:11 +0800

    fix: adjust GUI window positioning to occupy the right half of the screen

--- events/gui-startup.lua
+++ events/gui-startup.lua
@@ -6,7 +6,16 @@ local M = {}
 M.setup = function()
    wezterm.on('gui-startup', function(cmd)
       local _, _, window = mux.spawn_window(cmd or {})
-      window:gui_window():maximize()
+      local gui_window = window:gui_window()
+
+      -- Get the screen dimensions
+      local screen = wezterm.gui.screens().active
+      local screen_width = screen.width
+      local screen_height = screen.height
+
+      -- Set window to right half of screen
+      gui_window:set_position(screen_width / 2, 0)
+      gui_window:set_inner_size(screen_width / 2, screen_height)
    end)
 end
 
